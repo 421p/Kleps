43147cdea222230d12c5c72b7a58425022ec7130
421p <andrew.bakal@gmail.com>
2016-05-05 23:41:20 +0300

    game event data

--- GameEventSandbox/Program.cs
+++ GameEventSandbox/Program.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Kleps.Engine;
+using Kleps.Engine.Events;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace GameEventSandbox
 {
     class Program
     {
         static void Main(string[] args) {
+            Console.OutputEncoding = Encoding.UTF8;
             var backend = new Backend();
             backend.log("kek?");
 
             Console.WriteLine(backend.getGame().teacher.name);
             Console.WriteLine(backend.getGame().teacher.health);
+
+            var d = new Deserializer(namingConvention: new CamelCaseNamingConvention());
+            var obj = d.Deserialize<List<GameEventData>>(new StreamReader(new FileStream("../../../Kleps/DataRepository/Events.yaml", FileMode.Open)));
+
+            Console.WriteLine(obj[1].rightAnswer);
         }
     }
 }
---
+++ Kleps/Engine/Events/GameEventData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+namespace Kleps.Engine.Events
+{
+    public class GameEventData
+    {
+        public string text { get; set; }
+        public List<string> answers { get; set; }
+
+        [YamlAlias("right_answer")]
+        public string rightAnswer { get; set; }
+
+    }
+}
