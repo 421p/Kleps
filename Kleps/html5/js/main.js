757447b39c2c3ade37bbd837e899f51035a5a1bf
artemsky <mr.artemsky@gmail.com>
2016-05-06 15:30:55 +0300

    add game menu

--- Kleps/html5/js/main.js
+++ Kleps/html5/js/main.js
@@ -1,10 +1,23 @@
-﻿$(document).ready(function() {
-    $('button').click(function () {
-        backend.log($('textarea').val());
-    });
+﻿// $(document).ready(function() {
+//     $('button').click(function () {
+//         backend.log($('textarea').val());
+//     });
+//
+//     $('.hw').click(function() {
+//         alert(backend.hws.getHelloWorld());
+//     });
+// });
 
-    $('.hw').click(function() {
-        alert(backend.hws.getHelloWorld());
+$(function(){
+    $('select').change(function(){
+        console.log(123)
     });
+    $('select').on('mouseenter', 'option', function(e) {
+        $(this).css({'background':'red'});
+    });
+    $('select').on('mouseleave', 'option', function(e) {
+        $(this).css({'background':'none'});
+    });
+    $("#menu").val($("#menu option:first").val()).focus();
 });
 
