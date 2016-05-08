95bb4c762f02eb46181f91ce9d7297a478927114
artemsky <mr.artemsky@gmail.com>
2016-05-08 12:41:10 +0300

    add game logo

--- Kleps/html5/js/game.js
+++ Kleps/html5/js/game.js
@@ -1,4 +1,4 @@
 ﻿$(function () {
-    $('#timer-1').timer({ duration: 8, unit: 's' });
-    $('#timer-2').timer({ duration: 31, unit: 's' });
+    //$('#timer-1').timer({ duration: 8, unit: 's' });
+    //$('#timer-2').timer({ duration: 31, unit: 's' });
 })
