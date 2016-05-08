eb6ede80f7a463201b232689214ad4722e2be37a
artemsky <mr.artemsky@gmail.com>
2016-05-08 17:35:09 +0300

    adapt timer form 0s to 30s

--- Kleps/html5/js/game.js
+++ Kleps/html5/js/game.js
@@ -1,6 +1,6 @@
 ﻿$(function () {
     $('#timer-1').timer({ duration: 8, unit: 's' });
-    $('#timer-2').timer({ duration: 31, unit: 's' });
-    $('#timer-3').timer({ duration: 31, unit: 's' });
-    $('#timer-4').timer({ duration: 31, unit: 's' });
+    $('#timer-2').timer({ duration: 29, unit: 's' });
+    $('#timer-3').timer({ duration: 20, unit: 's' });
+    $('#timer-4').timer({ duration: 15, unit: 's' });
 })
