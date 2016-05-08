568b37507b5b5d92ed47d057d7bf70c21d93f757
artemsky <mr.artemsky@gmail.com>
2016-05-08 17:07:49 +0300

    add user listview

--- Kleps/html5/js/game.js
+++ Kleps/html5/js/game.js
@@ -1,4 +1,6 @@
 ﻿$(function () {
-    //$('#timer-1').timer({ duration: 8, unit: 's' });
-    //$('#timer-2').timer({ duration: 31, unit: 's' });
+    $('#timer-1').timer({ duration: 8, unit: 's' });
+    $('#timer-2').timer({ duration: 31, unit: 's' });
+    $('#timer-3').timer({ duration: 31, unit: 's' });
+    $('#timer-4').timer({ duration: 31, unit: 's' });
 })
