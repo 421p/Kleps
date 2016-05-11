52ad3deb6b74401b15066aaee10857d8d44de346
421p <andrew.bakal@gmail.com>
2016-05-11 19:41:12 +0300

    remove hws

--- Kleps/Engine/Backend.cs
+++ Kleps/Engine/Backend.cs
@@ -1,20 +1,17 @@
 ﻿using System;
 using System.Windows.Forms;
 using Kleps.Engine.Game;
-using Kleps.Engine.HelloWoldSayer;
 using Newtonsoft.Json;
 
 namespace Kleps.Engine {
 
     public class Backend {
         
-        public HelloWorldSayer hws { get; }
         public Game.Game game { get; private set; }
 
         public Gate Gate { get; private set; }
 
         public Backend(GameConfig cfg) {
-            hws = new HelloWorldSayer();
             game = GameFactory.CreateGame(cfg);
             Gate = new Gate(this);
         }
